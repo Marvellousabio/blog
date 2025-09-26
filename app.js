bc8d0925c798bc7a05d9f33bc4fb1c1a70b97454
marvellous ogunleke <marvellousabiola08@gmail.com>
2025-09-26 12:24:13 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,40 @@
-const express = require("express");
+const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+require('dotenv').config();
+const methodOverride = require('method-override');
+const blogRoutes = require('./routes/blogRoute.js');
+
 const app = express();
 
-app.use(express.json());
+// connect to mongo DB
+connectDB();
 
-app.get("/", (req, res) => {
-  res.json({ message: "Express on Vercel is working 🚀" });
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'));
+app.use(methodOverride('_method'));
+
+app.get('/', (req, res) => {
+  res.redirect('/blogs');
 });
+
+app.get('/about', (req, res) => {
+  res.render('about', { title: 'About' });
+});
+
+app.use('/blogs', blogRoutes);
+
+app.use((req, res) => {
+  res.status(404).render('404', { title: '404' });
+});
+
+app.get("/ping", (req, res) => {
+  res.send("pong");
+});
+
 
 module.exports = app;
